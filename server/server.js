5625a4c4a10dd0d85b88cf6cd9c57fefe823f7fc
Kameron Larsen <kroylar@gmail.com>
2015-12-26 20:26:32 -0800

    (#2): added initial rx packet parsing
    * removed unnecessary contents.txt
    * relocated master.json
    * now connecting to host:port, receiving packets, parsing them, and
      updating master.json

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
 // Run this server with: node server.js
 
-var filename = "../master.json",
+var filename = "master.json",
     port     = 8080,
+    rx_host  = 'localhost',
+    rx_port  = 8085,
     wsserv   = require('ws').Server,
     wss      = new wsserv({port: port}),
     fs       = require('fs'),
     ee       = require('event-emitter'),
     emitter  = ee({}), listener,
     csvParse = require('csv-parse'),
-    YAML     = require('yamljs');
+    YAML     = require('yamljs'),
+    net      = require('net');
 
-// Watch the file, emit 'file-changed' event on change.
-// Contents might not have changed. Could just be time.
-// TODO Check for content difference
-var watcher = fs.watch(filename, function (event, filename) {
-  if (event == 'change') {
-    emitter.emit('file-changed', filename);
-  }
-});
+var watcher;
 
 // Event handler for when the server recieves a connection
 wss.on('connection', function connection(ws) {
@@ -68,8 +64,8 @@ var generateJSON = function(layout_file, map_file, json_file) {
         // lofi[1] = location ID
         // lofi[2] = switch number
         master.push({
-          "lofi_number": lofi[0],
-          "lofi_switch": lofi[2],
+          "node_id": lofi[0],
+          "switch_id": lofi[2],
           "location_number": lofi[1],
           "location": layout.possibleLocations[lofi[1] - 1][lofi[1]].name,
           "type": layout.possibleLocations[lofi[1] - 1][lofi[1]].type,
@@ -80,8 +76,72 @@ var generateJSON = function(layout_file, map_file, json_file) {
         if (err) throw err;
         console.log('Saved JSON.');
       });
+
+      // Watch the file, emit 'file-changed' event on change.
+      // Contents might not have changed. Could just be time.
+      // TODO Check for content difference
+      watcher = fs.watch(filename, function (event, filename) {
+      if (event == 'change') {
+          emitter.emit('file-changed', filename);
+      }
+      });
     });
   });
 };
 
-generateJSON('../layout.conf', '../mapping.conf', '../master.json');
+generateJSON('../layout.conf', '../mapping.conf', filename);
+
+// Initiate a connection to the LoFi Receiver
+var client = net.connect(rx_port, rx_host, function() {
+  console.log('Connected to LoFi Receiver.');
+});
+
+// Called when receiving packets from the lofi receiver
+client.on('data', function(data) {
+  var idx;
+
+  var node_id;
+  var switch_id;
+  var sw_status;
+
+  console.log('Raw Data:\n' + data.toString('utf8'));
+  idx = data.indexOf('NodeId:');
+  node_id = parseInt(data.slice(idx+7, idx+10));
+  idx = data.indexOf('SW');
+  switch_id = parseInt(data.slice(idx+2,idx+3)) - 1;
+  sw_status = ((data.slice(idx+8, idx+12)).indexOf('O') == 0) ? 'open' : 'closed';
+
+  console.log('NodeId=' + node_id);
+  console.log('SwitchId=' + switch_id);
+  console.log('Status=' + sw_status);
+
+  var master;
+
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if(err)
+      throw err;
+
+    var update_json;
+    update_json = 0;
+    master = JSON.parse(data);
+    master.forEach(function(loc) {
+      if ((loc.node_id == node_id) && (loc.switch_id == switch_id) && (loc.status != sw_status)) {
+        if (sw_status == 1)
+          loc.status = 'open';
+        else
+          loc.status = 'closed';
+
+        update_json = 1;
+      }
+    });
+    if (update_json != 0) {
+      fs.writeFile(filename, JSON.stringify(master), function(err) {
+        if (err)
+          throw err;
+        console.log('updated JSON');
+      });
+    }
+  });
+});
+
+// vim: tabstop=2:shiftwidth=2:expandtab:
